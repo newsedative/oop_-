baa98f9dcaf9e5c5ef439ea6624f9cc5dd98db14
Darya <81254493+newsedative@users.noreply.github.com>
2024-10-03 14:30:39 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    //на 32-разрядных платформах размер адресов равен 4 байтам, а на 64-разрядных - 8 байтам
+
+    //task 1
+    int num = 12;
+    int *ptr_num = &num;
+    cout << "Значение указателя ptr_num - " << *ptr_num << ", Адрес указателя - " << ptr_num << endl;
+
+    bool flag = true;
+    bool* ptr_flag = &flag;
+    cout << "Значение указателя ptr_flag - " << *ptr_flag << ", Адрес указателя - " << ptr_flag << endl;
+
+    float fnum = 0.1;
+    float* ptr_fnum = &fnum;
+    cout << "Значение указателя ptr_fnum - " << *ptr_fnum << ", Адрес указателя - " << ptr_fnum << endl;
+
+
+    //task 2
+    enum days { Monday, Tuesday, Wednesday };
+    typedef days* ptr_days = &days;
+    cout << *ptr_days << endl;
+
+};
