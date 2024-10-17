270c510a60c635cc808b7e78c83f90543e1f2cb0
Darya <81254493+newsedative@users.noreply.github.com>
2024-10-17 14:43:49 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <random>
 using namespace std;
+
+int composition(int *arr, int size) {
+    int result = 1;
+    for (int i = 0; i < size; i++) {
+        if ((i + 1) % 2 == 0) {
+            result = result * arr[i];
+        };
+    }
+    return result;
+};
+
+
+int sum_of_first_and_end(int* arr, int size) {
+    int sum_without_zero = 0;
+    bool flag = false;
+
+    for (int i = 0; i < size; i++) {
+        if (flag) {
+            if (arr[i] == 0) {
+                break;
+            };
+            sum_without_zero += arr[i];
+        };
+        if (arr[i] == 0) {
+            flag = true;
+        };
+    };
+   return sum_without_zero;
+}
+
+typedef int (*PF)(int, int);
+
+void example(PF ptr_func, int* arr, int size) {
+    cout << "Вызов функции через указатель" << ptr_func(arr, size) << endl;
+}
 
 int main()
 {
@@ -34,7 +70,7 @@ int main()
         int Age;
         float Ball;
     };
-    Student s1 = {"Misha", 23, 8};
+    Student s1 = { "Misha", 23, 8 };
     Student* ptr_student = &s1;
     cout << "Имя: " << ptr_student->Name << " Возраст: " << (*ptr_student).Age << " Балл: " << (*ptr_student).Ball << endl;
     cout << endl;
@@ -43,7 +79,7 @@ int main()
     //task 4
     int number = 10;
     int* ptr = &number;
-    double* doublePtr = (double*)ptr; 
+    double* doublePtr = (double*)ptr;
     cout << ptr << ' ' << *doublePtr << endl;
     cout << endl;
 
@@ -70,18 +106,19 @@ int main()
     {
         cout << *ptr << " ";
     }
-    cout << endl;
+    cout << "\n" << "\n" << endl;
+
 
     //task 6
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(0, 30);
-    
+
     int rows = 4;
     int cols = 4;
-    
+
     int** dynamiс_mas{ new int* [rows] {} };
-    
+
     for (int i = 0; i < rows; i++) {
         dynamiс_mas[i] = new int[cols] {dist(gen), dist(gen), dist(gen), dist(gen)};
     }
@@ -92,7 +129,7 @@ int main()
         }
         cout << endl;
     }
-    
+
     cout << "Количество столбцов, содержащих хотя бы один нулевой элемент: ";
     int coutOfNull = 0;
     for (int i = 0; i < cols; i++) {
@@ -108,15 +145,22 @@ int main()
         }
     };
     cout << coutOfNull << endl;
-    
+
     for (int i = 0; i < rows; i++) {
         delete dynamiс_mas[i];
     }
     cout << endl;
-    
+
     
     //task 7
-    const int nums = 5;
-    int mas_task7[nums] = { 1, 2, 3, 4, 5 };
+    const int nums = 7;
+    int mas_task7[nums] = { 1, 2, 0, 4, 5, 6, 0 };
     cout << composition(mas_task7, nums) << endl;
+
+    //task 8
+    example(sum_of_first_and_end, mas_task7, nums);
+    /*Определить функцию, обрабатывающую массив по пункту А) и подпункту 2, передав его в качестве параметра. Объявить указатель на эту функцию и передать 
+    его некой другой функции, имеющей один из возможных параметров указатель на первую функцию.*/
+    /*2.	сумму элементов массива, расположенных между первым и последним нулевым  элементами;*/
+
 };
