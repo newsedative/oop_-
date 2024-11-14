3781e08d7d225988508dc4f9ee87639945f4c0d5
Darya <81254493+newsedative@users.noreply.github.com>
2024-11-14 14:33:38 +0300

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -28,7 +28,24 @@ void print_all_members() {
             cout << "Возраст: " << age << endl;
             cout << "Статус: " << status << endl;
         }
+        class FamilyMemberLocal {
+        public: 
+            double profit;
+            void print_profit() {
+                cout << "Доход: " << profit << endl;
+            }
+        };
     };
+
+    FamilyMember mom;
+    mom.surname = "Савельева";
+    mom.name = "Любовь";
+    mom.patronymic = "Ивановна";
+    mom.age = 50;
+    mom.status = "рабочий";
+    mom.profit = 12121212;
+    mom.print();
+    mom.print_profit();
 }
 
 
@@ -46,4 +63,5 @@ int main()
     mom.print();
 
     // task 2
+    print_all_members();
 }
