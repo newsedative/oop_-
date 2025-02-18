264faddba17aa192b944f3d0ae1c3c63f02a2421
Darya <81254493+newsedative@users.noreply.github.com>
2025-02-18 22:41:54 +0300

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,67 +1,46 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-class FamilyMember {
-public:
-    string surname;
+class FamilyMember
+{
+private:
     string name;
+    string surname;
     string patronymic;
     int age;
+    char gender;
     string status;
-    void print() {
-        cout << "ФИО: " << surname << " " << name << " " << patronymic << endl;
-        cout << "Возраст: " << age << endl;
-        cout << "Статус: " << status << endl;
-    }
-};
-
-void print_all_members() {
-    class FamilyMember {
-    public:
-        string surname;
-        string name;
-        string patronymic;
-        int age;
-        string status;
-        void print() {
-            cout << "ФИО: " << surname << " " << name << " " << patronymic << endl;
-            cout << "Возраст: " << age << endl;
-            cout << "Статус: " << status << endl;
-        }
-        class FamilyMemberLocal {
-        public: 
-            double profit;
-            void print_profit() {
-                cout << "Доход: " << profit << endl;
-            }
-        };
+public:
+    void input_member_info() {
+        cout << "Введите фамилию: ";
+        cin >> surname;
+        cout << "Введите имя: ";
+        cin >> name;
+        cout << "Введите отчество: ";
+        cin >> patronymic;
+        cout << "Введите возраст: ";
+        cin >> age;
+        cout << "Введите пол (М/Ж): ";
+        cin >> gender;
+        cout << "Введите статус (Студент/Рабочий/Пенсионер): ";
+        cin >> status;
     };
 
-    FamilyMember mom;
-    mom.surname = "Савельева";
-    mom.name = "Любовь";
-    mom.patronymic = "Ивановна";
-    mom.age = 50;
-    mom.status = "рабочий";
-    mom.profit = 12121212;
-    mom.print();
-    mom.print_profit();
-}
+    void print_member_info() {
+        cout << "Фамилия: " << surname << " Имя: " << name << " Отчество: " << surname
+            << " Возраст: " << age << " Пол: " << gender << " Статус: " << status << endl;
+    };
+};
 
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    //task 1
-    FamilyMember mom;
-    mom.surname = "Иванова";
-    mom.name = "Любовь";
-    mom.patronymic = "Ивановна";
-    mom.age = 50;
-    mom.status = "рабочий";
-    mom.print();
+    //task1
+    FamilyMember dad;
+    dad.input_member_info();
+    dad.print_member_info();
 
-    // task 2
-    print_all_members();
-}
+};
