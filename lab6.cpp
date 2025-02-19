7ea320ce7670f9c3a02985a2c8860dec37ceacc8
Darya <81254493+newsedative@users.noreply.github.com>
2025-02-19 14:41:45 +0300

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -2,16 +2,46 @@
 #include <iomanip>
 using namespace std;
 
+// task 1
 class FamilyMember
 {
 private:
+    class Salary
+    {
+    public:
+        // task 2
+        double salary;
+        Salary(double s_salary)
+        {
+            salary = s_salary;
+        };
+        void input_salary() {
+            cout << "Введите зарплату: ";
+            cin >> salary;
+        }
+        void print_salary() {
+            cout << "Зарплата: " << salary << endl;
+        };
+    };
     string name;
     string surname;
     string patronymic;
     int age;
     char gender;
     string status;
+    Salary salary;
 public:
+    // task 2, 3
+    FamilyMember(string f_name, string f_surname, string f_patronymic, int f_age, char f_gender, string f_status, double f_salary)
+    {
+        name = f_name;
+        surname = f_surname;
+        patronymic = f_patronymic;
+        age = f_age;
+        gender = f_gender;
+        status = f_status;
+        salary = Salary(f_salary);
+    };
     void input_member_info() {
         cout << "Введите фамилию: ";
         cin >> surname;
@@ -25,11 +55,18 @@ public:
         cin >> gender;
         cout << "Введите статус (Студент/Рабочий/Пенсионер): ";
         cin >> status;
+        salary.input_salary();
     };
 
     void print_member_info() {
-        cout << "Фамилия: " << surname << " Имя: " << name << " Отчество: " << surname
-            << " Возраст: " << age << " Пол: " << gender << " Статус: " << status << endl;
+        cout << "\n" << "Вывод информации: " << "\n" 
+            << "Фамилия: " << surname << "\n"
+            << "Имя: " << name << "\n" 
+            << "Отчество: " << surname << "\n"
+            << "Возраст: " << age << "\n" 
+            << "Пол: " << gender << "\n" 
+            << "Статус: " << status << endl;
+        salary.print_salary();
     };
 };
 
@@ -42,5 +79,10 @@ int main()
     FamilyMember dad;
     dad.input_member_info();
     dad.print_member_info();
+
+
+    //task 3
+    const FamilyMember mom = { "Inna", "Ivanova", "Ivanovna", 47, 'W', "Worker", 122222 };
+    mom.print_member_info();
 
 };
