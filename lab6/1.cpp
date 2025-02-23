a37c512e350d00c95fad405a26e8ede0884ba9be
Darya <81254493+newsedative@users.noreply.github.com>
2025-02-23 21:29:26 +0300

    Update and rename lab6.cpp to lab6/1.cpp

--- lab6/1.cpp
+++ lab6/1.cpp
@@ -6,42 +6,36 @@ using namespace std;
 class FamilyMember
 {
 private:
-    class Salary
-    {
-    public:
-        // task 2
-        double salary;
-        Salary(double s_salary)
-        {
-            salary = s_salary;
-        };
-        void input_salary() {
-            cout << "Введите зарплату: ";
-            cin >> salary;
-        }
-        void print_salary() {
-            cout << "Зарплата: " << salary << endl;
-        };
-    };
     string name;
     string surname;
     string patronymic;
     int age;
     char gender;
     string status;
+    class Salary
+    {
+    public:
+        // task 2
+        double income;
+        Salary() : income(0) {}
+        Salary(double s_income)
+        {
+            income = s_income;
+        };
+        void input_salary() {
+            cout << "Введите зарплату: ";
+            cin >> income;
+        }
+    };
     Salary salary;
 public:
+    FamilyMember() : name(""), surname(""), patronymic(""), age(0), gender(' '), status(""), salary(0) {};
     // task 2, 3
     FamilyMember(string f_name, string f_surname, string f_patronymic, int f_age, char f_gender, string f_status, double f_salary)
-    {
-        name = f_name;
-        surname = f_surname;
-        patronymic = f_patronymic;
-        age = f_age;
-        gender = f_gender;
-        status = f_status;
-        salary = Salary(f_salary);
-    };
+        : name(f_name), surname(f_surname), patronymic(f_patronymic), age(f_age), gender(f_gender), status(f_status), salary(f_salary) {}
+    // task 4
+    FamilyMember(string f_name, string f_surname, int f_age)
+        : name(f_name), surname(f_surname), patronymic(""), age(f_age), gender(' '), status(""), salary(0) {}
     void input_member_info() {
         cout << "Введите фамилию: ";
         cin >> surname;
@@ -58,15 +52,16 @@ public:
         salary.input_salary();
     };
 
-    void print_member_info() {
-        cout << "\n" << "Вывод информации: " << "\n" 
+    void print_member_info() const
+    {
+        cout << "\n" << "Вывод информации: " << "\n"
             << "Фамилия: " << surname << "\n"
-            << "Имя: " << name << "\n" 
+            << "Имя: " << name << "\n"
             << "Отчество: " << surname << "\n"
-            << "Возраст: " << age << "\n" 
-            << "Пол: " << gender << "\n" 
-            << "Статус: " << status << endl;
-        salary.print_salary();
+            << "Возраст: " << age << "\n"
+            << "Пол: " << gender << "\n"
+            << "Статус: " << status << "\n"
+            << "Зарплата: " << salary.income << endl;
     };
 };
 
@@ -84,5 +79,9 @@ int main()
     //task 3
     const FamilyMember mom = { "Inna", "Ivanova", "Ivanovna", 47, 'W', "Worker", 122222 };
     mom.print_member_info();
+
+    //task 4
+    FamilyMember son("Misha", "Mishin", 18);
+    son.print_member_info();
 
 };
