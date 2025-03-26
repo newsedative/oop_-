e1b64a4ffac7e166d70973c3c750b3d789ff2676
Darya <81254493+newsedative@users.noreply.github.com>
2025-03-26 12:31:35 +0300

    Update 6.cpp

--- lab6/6.cpp
+++ lab6/6.cpp
@@ -2,22 +2,63 @@
 #include <string>
 using namespace std;
 
+class Auto;
+
+class Person
+{
+    string name;
+public:
+    Person(string p_name): name(p_name) {}
+    void drive(const Auto&);
+};
+
 class Auto
 {
+    friend void Person::drive(const Auto&);
+private:
     string name;
     int price;
 public:
+    Auto(int a_price): name("none"), price(a_price) {}
     Auto(string a_name, int a_price): name(a_name), price(a_price) {}
-    friend void drive(const Auto&);
+    Auto(const Auto& a) {
+        cout << "constr of copy" << endl;
+        name = a.name;
+        price = a.price;
+    }
+    //delete
+    //Auto(const Auto& a) = delete;
+    Auto(Auto&& a) {
+        cout << "constr of move" << endl;
+        name = move(a.name);
+        price = a.price;
+        a.price = 0;
+    }
+    Auto operator + (const Auto& a) 
+    {
+        return Auto(price + a.price);
+    }
+    void print() {
+        cout << name << " " << price << endl;
+    }
 };
 
-void drive(const Auto& car) {
-    cout << car.name << " driven ";
+void Person::drive(const Auto& car) {
+    cout << name << " drive " << car.name << endl;
 }
 
 int main() {
     Auto mazda("mazda", 220);
-    drive(mazda);
+    Person dasha("dasha");
+    //dasha.drive(mazda);
+
+    Auto mazda6(mazda);
+    mazda6.print();
+
+    Auto movedMazda(mazda);
+
+    Auto plus = mazda + mazda6;
+    plus.print();
 
     return 0;
 }
