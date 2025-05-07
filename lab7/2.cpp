490a44253ec91867e64f234b52b3fadf7f40c611
Darya <81254493+newsedative@users.noreply.github.com>
2025-05-07 13:33:27 +0300

    Update 2.cpp

--- lab7/2.cpp
+++ lab7/2.cpp
@@ -24,8 +24,12 @@ protected:
     string surname;
     int age;
 public:
-    Human() : name(""), surname(""), age(0) {};
-    Human(string h_name, string h_surname, int h_age) : name(h_name), surname(h_surname), age(h_age) {};
+    Human() : name(""), surname(""), age(0) {
+        cout << "dfasfg";
+    };
+    Human(string h_name, string h_surname, int h_age) : name(h_name), surname(h_surname), age(h_age) {
+        cout << "dfasfg";
+    };
     virtual void print() =0;
 };
 
@@ -47,6 +51,8 @@ class Student : public Employee
     float medium_ball;
 public:
     Student() : group(""), medium_ball(0.0), Employee() {};
+    Student(string s_group, float s_medium_ball, string s_name, string s_surname, int s_age) :
+        group(s_group), medium_ball(s_medium_ball), Employee("", s_name, s_surname, s_age) {};
     Student(string s_group, float s_medium_ball, string s_specialization, string s_name, string s_surname, int s_age) :
         group(s_group), medium_ball(s_medium_ball), Employee(s_specialization, s_name, s_surname, s_age) {};
     void print() override {
@@ -61,8 +67,19 @@ int main() {
     setlocale(LC_ALL, "RUS");
 
 
-    Student dasha1("IVT-42-23", 4.7, "programming", "Dasha", "Mashinova", 20);
+    Student dasha1("IVT", 4.7, "programming", "name1", "surname1", 10);
     dasha1.print();
+
+    //преобразование
+    Employee* d = new Employee("software", "Ivan", "Ivanov", 22);
+    Student* s = static_cast<Student *> (d);
+    s->print();
+
+    Student* stud = new Student("ivt", 4.4, "name2", "surname2", 5);
+    Employee* stud1 = dynamic_cast<Employee*>(stud);
+    stud1->print();
+
+
 
     return 0;
 }
